c246f128c9f0ce476666156cdf14451c81eb35f1
Panji <panjiadhipramana@gmail.com>
2022-11-13 10:18:23 +0700

    [Panji] make card pelatihan

--- src/pages/DashboardAdmin/Pelatihan/index.jsx
+++ src/pages/DashboardAdmin/Pelatihan/index.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
+import React from "react";
 
 import Sidebar from "../../../components/Sidebar";
 import NavbarDashboard from "../../../components/Navbar/NavbarDashboard";
-import DaftarPelatihan from './DaftarPelatihan';
+import DaftarPelatihan from "./DaftarPelatihan";
+import PelatihanAktif from "./PelatihanAktif";
+import RiwayatPelatihan from "./Riwayatpelatihan";
 
 const Index = () => {
-    return (
-        <div className="flex bg-[#F9F9F9]">
-            <Sidebar />
-            <div className="w-full ml-64">
-                <NavbarDashboard judul="Management Juri" />
-                <div className="flex flex-col gap-5 mx-6 mt-20">
-                    <DaftarPelatihan judul="Tambah Pelatihan" link="/" />
-                </div>
-            </div>
+  return (
+    <div className="flex bg-[#F9F9F9] h-screen">
+      <Sidebar />
+      <div className="w-full ml-64">
+        <NavbarDashboard judul="Management Juri" />
+        <div className="flex flex-col gap-5 mx-6 mt-20">
+          <DaftarPelatihan judul="Tambah Pelatihan" link="/" />
+          <PelatihanAktif />
+          <RiwayatPelatihan />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Index
+export default Index;
