e382d0bcf421e1fa4db08a8a5d5313f05302b0f7
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2022-11-22 16:35:18 +0700

    [styles] fixing
    fixing and delete useless styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,51 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        poppins: ["Poppins", "sans-serif"],
+        poppins: ['Poppins', 'sans-serif'],
       },
       colors: {
-        "hijau": "#1ED747",
-        "primary": "#CD0706",
-        "secondary": "#F08619",
-        "biru": "#04A2FF",
-        "merah": "#EE1E14",
-        "hitam": "#2F3133",
-        "gray": "#BEBEBE",
-        "info": "#5CB3E4",
-        "warning": "#F6B675",
-        "error": "#E16868",
-        "success": "#6EBC14",
-        "neutral": "#79767B",
-      }
+        stp: {
+          primary: {
+            500: '#CD0606',
+            600: '#EE1E14',
+          },
+          secondary: '#F08619',
+          green: '#1ED747',
+          blue: '#04A2FF',
+          dark: {
+            500: '#292D32',
+          },
+          gray: '#BEBEBE',
+          info: '#5CB3E4',
+          warning: '#F6B675',
+          error: '#E16868',
+          success: '#6EBC14',
+          neutral: '#79767B',
+        },
+      },
+      spacing: {
+        '2px': '.125rem',
+        '3px': '.1875rem',
+      },
+    },
+    screens: {
+      xsm: '480px',
+      '2xsm': '576px',
+      sm: '640px',
+      md: '768px',
+      xmd: '840px',
+      '2md': '960px',
+      lg: '1024px',
+      xl: '1200px',
+      '2xl': '1440px',
+      '3xl': '1536px',
     },
   },
-  plugins: [require("daisyui")],
-  safelist: [{
-    pattern: /(bg|text|border-b)-(hijau|primary|secondary|biru|merah|gray|info|warning|error|success|neutral)/
-  }]
+  daisyui: {
+    themes: false,
+  },
+  plugins: [require('daisyui')],
 };
